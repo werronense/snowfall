6d5a9709d8302dafeb742e23cd02b25f7d4e580e
werronense <31091965+werronense@users.noreply.github.com>
2019-12-18 14:47:46 -0800

    Animate single snowflake

--- main.js
+++ main.js
@@ -3,10 +3,27 @@ window.onload = () => {
   let ctx = canvas.getContext('2d');
   let width = canvas.width = window.innerWidth;
   let height = canvas.height = window.innerHeight;
+  // temporary start point values
+  let x = 50;
+  let y = 50;
+  // temporary change rate values
+  let dx = 0;
+  let dy = 1.5;
 
-  ctx.beginPath();
-  ctx.arc(50, 50, 10, 0, Math.PI * 2);
-  ctx.fillStyle = 'white';
-  ctx.fill();
-  ctx.closePath();
+  function drawFlake(xi, yi) {
+    ctx.beginPath();
+    ctx.arc(xi, yi, 10, 0, Math.PI * 2);
+    ctx.fillStyle = 'white';
+    ctx.fill();
+    ctx.closePath();
+  }
+
+  function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawFlake(x, y);
+    x += dx;
+    y += dy;
+  }
+
+  setInterval(draw, 10);
 }
