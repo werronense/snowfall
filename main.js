72848e2cbca7ce870c6087b90e790ccb4c676463
werronense <31091965+werronense@users.noreply.github.com>
2019-12-18 14:32:08 -0800

    Resize canvas

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
-let canvas = document.getElementById('mainCanvas');
-let ctx = canvas.getContext('2d');
+window.onload = () => {
+  let canvas = document.getElementById('mainCanvas');
+  let ctx = canvas.getContext('2d');
+  let width = canvas.width = window.innerWidth;
+  let height = canvas.height = window.innerHeight;
+
+  ctx.beginPath();
+  ctx.arc(50, 50, 10, 0, Math.PI * 2);
+  ctx.fillStyle = 'white';
+  ctx.fill();
+  ctx.closePath();
+}
